e2817d8c80ff45436149c6937e7901e5c32e043d
Robin van den Udenhout <Robintjuh99@hotmail.com>
2018-04-17 12:43:48 +0200

    My first commit

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,18 @@
+package helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		int sneeze = 8;
+		int counter = 0;
+		
+		while (counter < sneeze){
+			System.out.println("Ha");
+			counter = counter + 1;
+		}
+		System.out.println("choo Hello Robin");
+		}
+		
+	}
+	
+
